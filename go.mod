801f39db21805c01cab40f79e8a3fa0401cbc564
kubemq <lior.nabat@kubemq.io>
2020-09-13 10:46:17 +0300

    update mod

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/kubemq-io/k8s
 
-go 1.13
+go 1.15
 
 require (
 	github.com/Masterminds/goutils v1.1.0 // indirect
@@ -11,6 +11,9 @@ require (
 	github.com/mitchellh/copystructure v1.0.0 // indirect
 	github.com/stretchr/testify v1.6.1
 	k8s.io/api v0.19.1
+	k8s.io/apiextensions-apiserver v0.18.6
 	k8s.io/apimachinery v0.19.1
+	k8s.io/client-go v0.19.1
 	sigs.k8s.io/controller-runtime v0.6.2
 )
+
---
+++ manager/manager.go
@@ -0,0 +1,21 @@
+package manager
+import (
+	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+
+	corev1alpha1 "github.com/kubemq-io/k8s/api/v1alpha1"
+	ext "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
+	// +kubebuilder:scaffold:imports
+)
+var (
+	scheme    = runtime.NewScheme()
+)
+func init() {
+	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+
+	utilruntime.Must(corev1alpha1.AddToScheme(scheme))
+
+	utilruntime.Must(ext.AddToScheme(scheme))
+
+}
