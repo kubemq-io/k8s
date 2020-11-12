3bf983377f0a580652a7af5aff936177d13fd1b3
kubemq <lior.nabat@kubemq.io>
2020-11-12 10:47:28 +0200

    release v1.0.4

--- controller/objects/manifests/default_connector.go
+++ controller/objects/manifests/default_connector.go
@@ -10,28 +10,28 @@ spec:
   type: bridges
   replicas: 1
   config: |-
-    bindings:
-      - name: bridges-example-binding
-        properties:
-          log_level: "info"
-        sources:
-          kind: source.events
-          name: cluster-sources
-          connections:
-            - address: "kubemq-cluster-grpc:50000"
-              client_id: "cluster-events-source"
-              channel: "events.source"
-              group:   ""
-              concurrency: "1"
-              auto_reconnect: "true"
-              reconnect_interval_seconds: "1"
-              max_reconnects: "0"
-        targets:
-          kind: target.events
-          name: cluster-targets
-          connections:
-            - address: "kubemq-cluster-grpc:50000"
-              client_id: "cluster-events-target"
-              channels: "events.target"
+    bindings: null
+`
+	DefaultConnectorTargetsManifest = `
+apiVersion: core.k8s.kubemq.io/v1alpha1
+kind: KubemqConnector
+metadata:
+  name: kubemq-bridges
+spec:
+  type: targets
+  replicas: 1
+  config: |-
+    bindings: null
+`
+	DefaultConnectorSourcesManifest = `
+apiVersion: core.k8s.kubemq.io/v1alpha1
+kind: KubemqConnector
+metadata:
+  name: kubemq-bridges
+spec:
+  type: sources
+  replicas: 1
+  config: |-
+    bindings: null
 `
 )
