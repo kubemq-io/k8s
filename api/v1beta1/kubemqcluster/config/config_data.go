be6629cd6984dd0b512387c2942a578c480df1d3
Lior Nabat <lior.nabat@gmail.clom>
2023-05-24 15:31:10 +0300

    update oidc

---
+++ api/v1beta1/kubemqcluster/config/config_data.go
@@ -0,0 +1,64 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/kubemq-io/k8s/api/v1beta1/kubemqcluster/deployment"
+)
+
+type OIDCConfig struct {
+	Issuer                     string `json:"issuer"`
+	ClientID                   string `json:"clientID"`
+	SkipClientIDCheck          bool   `json:"skipClientIDCheck"`
+	SkipExpiryCheck            bool   `json:"skipExpiryCheck"`
+	SkipIssuerCheck            bool   `json:"skipIssuerCheck"`
+	InsecureSkipSignatureCheck bool   `json:"insecureSkipSignatureCheck"`
+}
+
+func (o *OIDCConfig) String() string {
+	b, _ := json.Marshal(o)
+	return string(b)
+}
+func (o *OIDCConfig) Validate() error {
+	if o.Issuer == "" {
+		return fmt.Errorf("issuer is required")
+	}
+	if o.ClientID == "" {
+		return fmt.Errorf("client_id is required")
+	}
+	return nil
+}
+
+type AdditionalDataConfiguration struct {
+	Oidc *OIDCConfig `json:"oidc,omitempty"`
+}
+
+func NewAdditionalDataConfiguration() *AdditionalDataConfiguration {
+	return &AdditionalDataConfiguration{}
+}
+
+func (adc *AdditionalDataConfiguration) Unmarshal(configData string) error {
+	if err := json.Unmarshal([]byte(configData), adc); err != nil {
+		return fmt.Errorf("error unmarshaling config data, %s", err.Error())
+	}
+	if adc.Oidc != nil {
+		if err := adc.Oidc.Validate(); err != nil {
+			return fmt.Errorf("error validating oidc config, %s", err.Error())
+		}
+	}
+	return nil
+}
+
+func (adc *AdditionalDataConfiguration) SetConfig(config *deployment.Config) *AdditionalDataConfiguration {
+	if adc.Oidc != nil {
+		secConfig, ok := config.ConfigMaps[config.Name]
+		if ok {
+			if adc.Oidc != nil {
+				secConfig.SetStringVariable("AUTHENTICATION_ENABLE", "true").
+					SetStringVariable("AUTHENTICATION_TYPE", "oidc").
+					SetDataVariable("AUTHENTICATION_CONFIG", adc.Oidc.String())
+			}
+		}
+	}
+	return adc
+}
